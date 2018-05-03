907f33ab078e9945e6f356b6d9851cfe93144019
Jaroslav Klaus <buzz3333@gmail.com>
2018-05-03 17:10:44 +0200

    Add own in memory inverted index Add search in own index

--- src/main/java/cz/zcu/kiv/nlp/ir/indexer/Index.java
+++ src/main/java/cz/zcu/kiv/nlp/ir/indexer/Index.java
@@ -1,48 +1,48 @@
 package cz.zcu.kiv.nlp.ir.indexer;
 
+import cz.zcu.kiv.nlp.ir.indexer.model.IndexDto;
+import cz.zcu.kiv.nlp.ir.preprocessing.AdvancedTokenizer;
+import cz.zcu.kiv.nlp.ir.preprocessing.CzechStemmerLight;
+import cz.zcu.kiv.nlp.ir.preprocessing.Stemmer;
 import cz.zcu.kiv.nlp.ir.trec.data.Document;
 import cz.zcu.kiv.nlp.ir.trec.data.Result;
 import cz.zcu.kiv.nlp.ir.trec.data.ResultImpl;
 import org.apache.log4j.Logger;
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.CharArraySet;
-import org.apache.lucene.analysis.cz.CzechAnalyzer;
-import org.apache.lucene.document.Field;
-import org.apache.lucene.document.StringField;
-import org.apache.lucene.document.TextField;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.queryparser.classic.QueryParser;
-import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.Query;
-import org.apache.lucene.search.ScoreDoc;
-import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * @author tigi
  */
-
 public class Index implements Indexer, Searcher {
 
 	private static Logger log = Logger.getLogger(Index.class);
 
-	private Analyzer analyzer;
+	/*private Analyzer analyzer;
 
-	private Directory indexDir;
+	private Directory indexDir;*/
+
+	private Map<String, IndexDto> index;
+
+	private Stemmer stemmer;
+
+	private Analyzer queryAnalyzer = new StandardAnalyzer();
 
 	public void index(List<Document> documents) {
 		try {
-			analyzer = new CzechAnalyzer(new CharArraySet(Files.readAllLines(new File("stopwords.txt").toPath()),
+			/*analyzer = new CzechAnalyzer(new CharArraySet(Files.readAllLines(new File("stopwords.txt").toPath()),
 					true));
 			indexDir = FSDirectory.open(new File("index").toPath());
 
@@ -52,13 +52,47 @@ public class Index implements Indexer, Searcher {
 				addDocs(w, documents);
 
 				w.close();
+			}*/
+
+			if (index == null) {
+				index = new HashMap<>();
 			}
-		} catch (IOException e) {
+
+			if (stemmer == null) {
+				stemmer = new CzechStemmerLight();
+			}
+
+			for (Document d : documents) {
+				String text = d.getDate().toString() + ' ' + d.getTitle() + ' ' + d.getText();
+				String[] tokens = tokenize(text);
+
+
+				for (String token : tokens) {
+					if (index.containsKey(token)) {
+						index.get(token).addDoc(d);
+					} else {
+						IndexDto indexDto = new IndexDto(token, d, documents.size());
+						index.put(token, indexDto);
+					}
+				}
+			}
+		} catch (/*IO*/Exception e) {
 			log.error(e);
 		}
 	}
 
-	private void addDocs(IndexWriter w, List<Document> documents) throws IOException {
+	private String[] tokenize(String text) {
+		List<String> retVal = new LinkedList<>();
+		String[] tokens = AdvancedTokenizer.tokenize(text, AdvancedTokenizer.DEFAULT_REGEX);
+		for (String token : tokens) {
+			String t = stemmer.stem(token);
+			retVal.add(AdvancedTokenizer.removeAccents(t));
+		}
+
+		return retVal.toArray(new String[0]);
+	}
+
+	/*private void addDocs(IndexWriter w, List<Document> documents) throws IOException {
 		List<org.apache.lucene.document.Document> docs = new LinkedList<>();
 		for (Document d : documents) {
 			org.apache.lucene.document.Document luceneDocument = new org.apache.lucene.document.Document();
@@ -71,14 +105,14 @@ public class Index implements Indexer, Searcher {
 		}
 
 		w.addDocuments(docs);
-	}
+	}*/
 
 	public List<Result> search(String query) {
 		List<Result> results = new LinkedList<>();
 
-		try (IndexReader reader = DirectoryReader.open(indexDir)) {
+		/*try (IndexReader reader = DirectoryReader.open(indexDir)) {
 			int hitsPerPage = 10;
-			Query q = new QueryParser("text", analyzer).parse(query); // TODO: Klaus - Is "text" correct?
+			Query q = new QueryParser("text", analyzer).parse(query);
 			IndexSearcher searcher = new IndexSearcher(reader);
 			TopDocs docs = searcher.search(q, hitsPerPage);
 			ScoreDoc[] hits = docs.scoreDocs;
@@ -92,11 +126,100 @@ public class Index implements Indexer, Searcher {
 //				result.setRank(); FIXME: Klaus - Set value although it seems it's not needed.
 
 				results.add(result);
-			}
+			}*/
+		try {
+			Query q = new QueryParser("", queryAnalyzer).parse(query);
+			List<IndexDto> required = new LinkedList<>();
+			List<IndexDto> nonrequired = new LinkedList<>();
+			List<IndexDto> prohibited = new LinkedList<>();
+			extractQuery(q, required, nonrequired, prohibited);
+
+			Set<Document> docs = conjunctRequired(required);
+
+			addNonrequired(nonrequired, docs);
+
+			removeProhibited(prohibited, docs);
+
+			results = createResults(docs);
 		} catch (Exception e) {
 			log.error(e);
 		}
 
 		return results;
+	}
+
+	private List<Result> createResults(Set<Document> docs) {
+		List<Result> results = new LinkedList<>();
+		for (Document hit : docs) {
+			ResultImpl result = new ResultImpl();
+			result.setDocumentID(hit.getId());
+			result.setScore(1);
+//				result.setRank(); FIXME: Klaus - Set value although it seems it's not needed.
+
+			results.add(result);
+		}
+		if (results.size() > 10) {
+			results.sort(Comparator.comparing(Result::getScore).reversed());
+			results = results.subList(0, 10);
+		}
+		return results;
+	}
+
+	private void removeProhibited(List<IndexDto> prohibited, Set<Document> docs) {
+		for (IndexDto indexDto : prohibited) {
+			if (docs.isEmpty()) {
+				break;
+			}
+
+			docs.removeAll(indexDto.getDocs());
+		}
+	}
+
+	private void addNonrequired(List<IndexDto> nonrequired, Set<Document> docs) {
+		for (IndexDto indexDto : nonrequired) {
+			docs.addAll(indexDto.getDocs());
+		}
+	}
+
+	private Set<Document> conjunctRequired(List<IndexDto> required) {
+		required.sort(Comparator.comparingInt(o -> o.getDocs().size()));
+		Set<Document> docs = new TreeSet<>(Comparator.comparing(Document::getId));
+		if (required.size() >= 2) {
+			IndexDto first = required.get(0);
+			IndexDto second = required.get(1);
+
+			Set<Document> firstSet = new TreeSet<>(Comparator.comparing(Document::getId));
+			firstSet.addAll(first.getDocs());
+			firstSet.retainAll(second.getDocs());
+			docs.addAll(firstSet);
+
+			for (int i = 2; i < required.size(); i++) {
+				if (docs.isEmpty()) {
+					break;
+				}
+
+				docs.retainAll(required.get(i).getDocs());
+			}
+		} else if (required.size() == 1) {
+			docs.addAll(required.get(0).getDocs());
+		}
+		return docs;
+	}
+
+	private void extractQuery(Query q, List<IndexDto> required, List<IndexDto> nonrequired, List<IndexDto>
+			prohibited) {
+		for (BooleanClause clause : ((BooleanQuery) q).clauses()) {
+			String token = tokenize(clause.toString())[0];
+			IndexDto indexDto = index.get(token);
+			if (indexDto != null) {
+				if (clause.isProhibited()) {
+					prohibited.add(indexDto);
+				} else if (clause.isRequired()) {
+					required.add(indexDto);
+				} else {
+					nonrequired.add(indexDto);
+				}
+			}
+		}
 	}
 }
---
+++ src/main/java/cz/zcu/kiv/nlp/ir/indexer/model/IndexDto.java
@@ -0,0 +1,37 @@
+package cz.zcu.kiv.nlp.ir.indexer.model;
+
+import cz.zcu.kiv.nlp.ir.trec.data.Document;
+import lombok.Data;
+
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * @author Jaroslav Klaus
+ */
+@Data
+public class IndexDto {
+
+	String term;
+
+	Set<Document> docs;
+
+	Integer allDocsCount;
+
+	public IndexDto(String term, Document document, Integer allDocsCount) {
+		this.term = term;
+		this.allDocsCount = allDocsCount;
+		docs = new TreeSet<>(Comparator.comparing(Document::getId));
+		docs.add(document);
+	}
+
+	public double getIdf() {
+		return allDocsCount / (double) docs.size();
+	}
+
+	public void addDoc(Document document) {
+		docs.add(document);
+	}
+
+}
